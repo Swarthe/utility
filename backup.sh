908f8a7219bc5c12067822407159809588d98e33
Svert <emil.a.overbeck@gmail.com>
2021-08-29 17:34:21 +0200

    printf and better colour management

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #
-# backup: Backup the filesystem to an external location, assuming FHS compliance
+# backup: Synchronise the filesystem to an external location with rsync, assuming FHS compliance
 #
 # Copyright (c) 2021 Emil Overbeck <https://github.com/Swarthe>
-# Licensed under the MIT License. See LICENSE.txt for more information.
+#
+# Subject to the MIT License. See LICENSE.txt for more information.
 #
 
 # todo
@@ -12,12 +13,13 @@
 # test thoroughly on linux, and backup desktop to make sure BACKUP_TARGET and messages and checks and backup itself work properly (especially free space check and drive model and latest mounted deduction)
 
 #
-# User I/O functions 
+# User I/O functions and variables
 #
-usage () {
+usage ()
+{
     cat << EOF
 Usage: backup [OPTION]... [-t] [TARGET]
-Backup the filesystem to an external location.
+Synchronise the filesystem to an external location.
 Unless manually set, automatically determine the target.
 
 Options:
@@ -34,25 +36,37 @@ Note: Export the 'BACKUP_TARGET' variable to set the default target.
 EOF
 }
 
-err () {
-    echo -e "\e[1;31merror:\033[0m $*" >&2
+if [ $(tput colors) -ge 256 ]; then
+    readonly bold_red="\e[1;31m"
+    readonly bold_yellow="\e[1;33m"
+    readonly bold_blue="\e[1;34m"
+    readonly bold_cyan="\e[1;36m"
+    readonly normal="\033[0m"
+fi
+
+err ()
+{
+    printf "%berror:%b $*\n" "$bold_red" "$normal" >&2
 }
 
-warn () {
-    echo -e "\e[1;33mwarn:\033[0m $*"
+warn ()
+{
+    printf "%bwarn:%b $*\n" "$bold_yellow" "$normal" >&2
 }
 
-info () {
-    echo -e "\e[1;34minfo:\033[0m $*" 
+info ()
+{
+    printf "%binfo:%b $*\n" "$bold_blue" "$normal"
 }
 
-ask () {
-    local confirm=0
-    until [ "$confirm" = "y" -o "$confirm" = "n" -o -z "$confirm" ]; do
-        echo -ne "\e[1;36m::\e[0m $* [y/n] "
-        read confirm
+ask ()
+{
+    local confirm
+    until [ "$confirm" = "y" -o "$confirm" = "n" ]; do
+        printf "%b::%b $* [y/n] " "$bold_cyan" "$normal"
+        read -r confirm
     done
-    [ "$confirm" != "y" -a ! -z "$confirm" ] && exit
+    [ "$confirm" != "y" ] && return 1 || return 0
 }
 
 #
@@ -60,34 +74,34 @@ ask () {
 #
 while getopts :hvlist: opt; do
     case "${opt}" in
-        h) 
-            usage && exit
-            ;;
-        v)
-            verbose=1
-            ;;
-        l)
-            log=1
-            ;;
-        i)
-            interact=1
-            ;;
-        t)
-            target="$(realpath "$OPTARG")"
-            ;;
-        s)
-            skip_interact=1
-            ;;
-        :)
-            err "Option '$OPTARG' requires an argument"
-            echo "Try 'backup -h' for more information."
-            exit 1
-            ;;
-        \?)
-            err "Invalid option '$OPTARG'"
-            echo "Try 'backup -h' for more information."
-            exit 1
-            ;;
+    h)
+        usage && exit
+        ;;
+    v)
+        verbose=1
+        ;;
+    l)
+        log=1
+        ;;
+    i)
+        interact=1
+        ;;
+    t)
+        target="$(realpath "$OPTARG")"
+        ;;
+    s)
+        skip_interact=1
+        ;;
+    :)
+        err "Option '$OPTARG' requires an argument"
+        printf '%s\n' "Try 'backup -h' for more information."
+        exit 1
+        ;;
+    \?)
+        err "Invalid option '$OPTARG'"
+        printf '%s\n' "Try 'backup -h' for more information."
+        exit 1
+        ;;
     esac
 done
 
@@ -95,50 +109,58 @@ done
 # Attempt to determine target and related data if needed
 #
 latest_source="$(df --output=source | tail -n 1)"
-target_target="$(df --output=target "$target" | tail -n 1)"
 
 if [ -z "$target" ]; then
-    if [ -n "$BACKUP_TARGET" ]; then
+    if [ "$BACKUP_TARGET" ]; then
         target="$(realpath "$BACKUP_TARGET")"
     # use latest mounted real filesystem
     elif [ -e "$latest_source" ]; then
         target="$(realpath "$latest_source")"
     else
         err "Could not determine a suitable target"
-        echo "Try 'backup -h' for more information."
+        printf '%s\n' "Try 'backup -h' for more information."
         exit 1
     fi
 fi
 
-target_model="$(lsblk -no MODEL /dev/"$(lsblk -no PKNAME \
-"$(df --output=source "$target" 2> /dev/null | tail -n 1)" \
-2> /dev/null)" 2> /dev/null)"
+[ -e "$target" ] \
+    && target_source="$(df --output=source "$target" | tail -n 1)"
+
+[ "$target_source" ] \
+    && target_target="$(df --output=target "$target" | tail -n 1)"
+
+[ "$target_source" ] \
+    && target_model="$(lsblk -no MODEL /dev/"$(lsblk -no PKNAME "$target_source")")"
 
 #
 # Run checks
 #
-check_root () {
+check_root ()
+{
     [ "$target_target" != "/" ]
 }
 
-check_space () {
+check_space ()
+{
     [ $(df --output=used -k / | tail -n 1) \
-    -lt $(df --output=avail -k "$target" | tail -n 1) ]
+    -lt $(df --output=size -k "$target" | tail -n 1) ]
 }
 
 if [ -z "$interact" ]; then
     if [ "$(id -u)" != "0" ]; then
         warn "We do not have root privileges"
     fi
-    if [ -d "$target" -a -w "$target" -a -n "$target_model" ]; then
+
+    if [ -d "$target" -a -w "$target" -a "$target_model" ]; then
         if ! check_root; then
             warn "Target drive '$target_model' is the root drive"
             target_is_root=1
         fi
+
         if ! check_space; then
             warn "Target drive '$target_model' has insufficient free space"
         fi
-    elif [ -d "$target" -a -n "$target_model" ]; then
+    elif [ -d "$target" -a "$target_model" ]; then
         warn "Target '$target' is inaccessible"
     elif [ -e "$target" ]; then
         warn "Target '$target' is invalid"
@@ -147,22 +169,30 @@ if [ -z "$interact" ]; then
     fi
 else
     if [ "$(id -u)" != "0" ]; then
-        ask "We do not have root privileges. Proceed anyway?"
+        ask "We do not have root privileges. Proceed anyway?" \
+            || exit 0
     fi
-    if [ -d "$target" -a -w "$target" -a -n "$target_model" ]; then
+
+    if [ -d "$target" -a -w "$target" -a "$target_model" ]; then
         if ! check_root; then
-            ask "Target drive '$target_model' is the root drive. Proceed anyway?"
+            ask "Target drive '$target_model' is the root drive. Proceed anyway?" \
+                || exit 0
             target_is_root=1
         fi
+
         if ! check_space; then
-            ask "Target drive '$target_model' has insufficient free space. Proceed anyway?"
+            ask "Target drive '$target_model' has insufficient free space. Proceed anyway?" \
+                || exit 0
         fi
-    elif [ -d "$target" -a -n "$target_model" ]; then
-        ask "Target '$target' is inaccessible. Proceed anyway?"
+    elif [ -d "$target" -a "$target_model" ]; then
+        ask "Target '$target' is inaccessible. Proceed anyway?" \
+            || exit 0
     elif [ -e "$target" ]; then
-        ask "Target '$target' is invalid. Proceed anyway?"
+        ask "Target '$target' is invalid. Proceed anyway?" \
+            || exit 0
     else
-        ask "Target '$target' does not exist. Proceed anyway?"
+        ask "Target '$target' does not exist. Proceed anyway?" \
+            || exit 0
     fi
 fi
 
@@ -170,40 +200,44 @@ fi
 # Confirm or announce target
 #
 if [ -z "$skip_interact" ]; then
-    if [ -n "$target_model" ]; then
-        ask "Backup to '$target' on '$target_model'?"
+    if [ "$target_model" ]; then
+        ask "Backup to '$target' on '$target_model'?" \
+            || exit 0
     else
-        ask "Backup to '$target'?"
+        ask "Backup to '$target'?" \
+            || exit 0
     fi
 else
-    if [ -n "$target_model" ]; then
-        info "Target is '$target' on '$target_model'"
+    if [ "$target_model" ]; then
+        info "Target is '$target' on '$target_model'" \
+            || exit 0
     else
-        info "Target is '$target'"
+        info "Target is '$target'" \
+            || exit 0
     fi
 fi
 
 #
 # Run backup
 #
-if [ -z "$verbose" -a -z "$log" ]; then
-    syncr () {
+syncr ()
+{
+    if [ -z "$verbose" -a -z "$log" ]; then
         rsync -aHAX --info=progress2 --delete / \
         --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/lost+found","/swapfile"} \
         "$target"
-    }
-else
-    syncr () {
+    else
         rsync -aHAXv --delete / \
         --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/lost+found","/swapfile"} \
         "$target"
-    }
+    fi
+}
 
-fi
-
-clean () {
-    if [ -z "$skip_interact" -a -z "$target_is_root" -a -n "$target_model" ]; then
-        ask "Unmount target drive '$target_model' at '$target_target'?"
+clean ()
+{
+    if [ -z "$skip_interact" -a -z "$target_is_root" -a "$target_model" ]; then
+        ask "Unmount target drive '$target_model' at '$target_target'?" \
+            || exit 0
         umount "$target_target"
     fi
 }
@@ -214,24 +248,30 @@ else
     if [ -w . ]; then
         syncr &> "backup.log" &
         rsync_pid=$!
-        info "Log file is '"$(realpath backup.log)"'"
+        info "Log file is '$(realpath backup.log)'"
     else
         syncr &> /dev/null &
         rsync_pid=$!
         err "Could not create log file"
     fi
+
     while kill -0 $rsync_pid 2> /dev/null; do
-        for i in / - \\ \|; do 
-            echo -ne "\r\e[K\e[1;34minfo:\033[0m Backup in progress $i"
-            sleep 0.1
+        for i in . .. ...; do
+            # the escape code resets the line
+            printf "\r\e[K%binfo:%b Backup in progress%b" "$bold_blue" \
+                                                          "$normal" \
+                                                          "$i"
+            sleep 0.5
         done
     done
-    echo
+
+    printf "\n"
+
     if wait $rsync_pid; then
-        info "Backup successful"
-        clean
+        info "Backup successful" && clean
     else
         err "Backup failed"
-        [ -e backup.log ] && echo "See 'backup.log' for more information."
+        [ -e backup.log ] \
+            && printf '%s\n' "See 'backup.log' for more information."
     fi
 fi
--- scot.sh
+++ scot.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 #
-# scot: Capture the display to clipboard or file (intended for use with window managers)
+# scot: Capture the display to clipboard or file with magick import
+# (intended for use with window managers)
 #
 # Copyright (c) 2021 Emil Overbeck <https://github.com/Swarthe>
-# Licensed under the MIT License. See LICENSE.txt for more information.
+#
+# Subject to the MIT License. See LICENSE.txt for more information.
 #
 
 #
-# User I/O functions 
+# User I/O functions and variables
 #
-usage () {
+if [ $(tput colors) -ge 256 ]; then
+    readonly bold_red="\e[1;31m"
+    readonly bold_blue="\e[1;34m"
+    readonly normal="\033[0m"
+fi
+
+usage ()
+{
     cat << EOF
 Usage: scot [OPTION]
-Capture the display to clipboard or file.
+Capture the display.
 Unless overriden, capture rectangular selection.
 
 Options:
@@ -27,12 +36,14 @@ Note: Export the 'SCOT_TARGET' variable to set the target for '-f'.
 EOF
 }
 
-info () {
-    echo -e "\e[1;34minfo:\033[0m $*" 
+err ()
+{
+    printf "%berror:%b $*\n" "$bold_red" "$normal" >&2
 }
 
-err () {
-    echo -e "\e[1;31merror:\033[0m $*" >&2
+info ()
+{
+    printf "%binfo:%b $*\n" "$bold_blue" "$normal"
 }
 
 #
@@ -40,30 +51,33 @@ err () {
 #
 while getopts :hdgf opt; do
     case "${opt}" in
-        h)
-            usage; exit
-            ;;
-        d)
-            display="1"
-            ;;
-        g)
-            gui="1"
-            # change i/o functions to graphical output
-            err () {
-                notify-send -u critical "error" "$*"
-            }
-            info () {
-                notify-send -i "$target" "info" "$*"
-            }
-            ;;
-        f)
-            file="1"
-            ;;
-        \?)
-            err "Invalid option '$OPTARG'"
-            echo "Try 'scot -h' for more information."
-            exit 1
-            ;;
+    h)
+        usage; exit
+        ;;
+    d)
+        display="1"
+        ;;
+    g)
+        graphical="1"
+
+        err ()
+        {
+            notify-send -u critical "scot" "$*"
+        }
+
+        info ()
+        {
+            notify-send -i "$target" "scot" "$*"
+        }
+        ;;
+    f)
+        file="1"
+        ;;
+    \?)
+        err "Invalid option '$OPTARG'"
+        printf '%s\n' "Try 'scot -h' for more information."
+        exit 1
+        ;;
     esac
 done
 
@@ -78,19 +92,20 @@ if [ -z "$file" ]; then
     fi
 else
     # increment filename to avoid overwriting
-    if [ -n "$SCOT_TARGET" ]; then
+    if [ "$SCOT_TARGET" ]; then
         target="$(realpath "${SCOT_TARGET}/screenshot.png")"
         while [ -e "$target" ]; do
-            i=$(($i + 1))
+            i+=1
             target="$(realpath "${SCOT_TARGET}/screenshot-${i}.png")"
         done
     else
         target="$(realpath screenshot.png)"
         while [ -e "$target" ]; do
-            i=$(($i + 1))
+            i+=1
             target="$(realpath screenshot-${i}.png)"
         done
     fi
+
     if [ -z "$display" ]; then
         import "$target" 2> /dev/null
     else
@@ -101,7 +116,7 @@ fi
 #
 # Announce or notify
 #
-if [ -n "$gui" ]; then
+if [ "$graphical" ]; then
     if [ -z "$file" ]; then
         info "Screenshot successful"
     elif [ -e "$target" ]; then
@@ -109,6 +124,10 @@ if [ -n "$gui" ]; then
     else
         err "Could not save screenshot"
     fi
-else
-    [ -e "$target" ] && info "Screenshot is '$target'"
+elif [ "$file" ]; then
+    if [ -e "$target" ]; then
+        info "Screenshot is '$target'"
+    else
+        err "Could not save screenshot"
+    fi
 fi
